800591c8b8ac3f5b0c43027522785cc5adbd793f
majd mahmoud <majd.mahmoud161@gmail.com>
2026-05-09 13:27:28 +0300

    Require one and tow done ✅

--- MultiMediaProject/Form1.Designer.cs
+++ MultiMediaProject/Form1.Designer.cs
@@ -30,6 +30,11 @@
         {
             button1 = new Button();
             pictureBox1 = new PictureBox();
+            button2 = new Button();
+            button4 = new Button();
+            button3 = new Button();
+            button5 = new Button();
+            button6 = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
@@ -55,11 +60,66 @@
             pictureBox1.DragDrop += pictureBox1_DragDrop;
             pictureBox1.DragEnter += pictureBox1_DragEnter;
             // 
+            // button2
+            // 
+            button2.Location = new Point(1141, 98);
+            button2.Name = "button2";
+            button2.Size = new Size(171, 75);
+            button2.TabIndex = 2;
+            button2.Text = "RGB_To_HSV";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(1141, 179);
+            button4.Name = "button4";
+            button4.Size = new Size(171, 71);
+            button4.TabIndex = 4;
+            button4.Text = "RGB_To_YCBCR";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(1141, 321);
+            button3.Name = "button3";
+            button3.Size = new Size(171, 55);
+            button3.TabIndex = 5;
+            button3.Text = "RGB_To_LAB";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(1141, 256);
+            button5.Name = "button5";
+            button5.Size = new Size(171, 59);
+            button5.TabIndex = 6;
+            button5.Text = "RGB_To_YUV";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(1141, 382);
+            button6.Name = "button6";
+            button6.Size = new Size(171, 51);
+            button6.TabIndex = 7;
+            button6.Text = "RGB_To_CMYK";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1388, 746);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button3);
+            Controls.Add(button4);
+            Controls.Add(button2);
             Controls.Add(pictureBox1);
             Controls.Add(button1);
             Name = "Form1";
@@ -72,5 +132,10 @@
 
         private Button button1;
         private PictureBox pictureBox1;
+        private Button button2;
+        private Button button4;
+        private Button button3;
+        private Button button5;
+        private Button button6;
     }
 }
--- MultiMediaProject/Form1.cs
+++ MultiMediaProject/Form1.cs
@@ -3,6 +3,7 @@ using Emgu.CV.Structure;
 using System;
 using System.Windows.Forms;
 using System.IO;
+using Emgu.CV.CvEnum;
 
 
 namespace MultiMediaProject
@@ -11,31 +12,25 @@ namespace MultiMediaProject
     {
 
         Bitmap originalImage;
-        
+
         public Form1()
         {
             InitializeComponent();
             pictureBox1.AllowDrop = true;
 
         }
-
+        //تحميل الصورة من  اللابتوب عالبرنامج
         private void button1_Click(object sender, EventArgs e)
         {
-            //youtube
-            //OpenFileDialog openfiledealog = new OpenFileDialog();
-            //DialogResult re = openfiledealog.ShowDialog();
-            //if (re==DialogResult.OK) {
-            //    i = Image.FromFile(openfiledealog.FileName);
-            //    b = (Bitmap)i;
-            //    pictureBox1 .Image = b;
 
-
-            //}
             OpenFileDialog openfiledealog = new OpenFileDialog();
-            if (openfiledealog.ShowDialog()==DialogResult.OK) {
+            if (openfiledealog.ShowDialog() == DialogResult.OK)
+            {
                 originalImage = new Bitmap(openfiledealog.FileName);
                 pictureBox1.Image = originalImage;
+
             }
+
 
         }
 
@@ -46,6 +41,7 @@ namespace MultiMediaProject
 
         private void pictureBox1_DragEnter(object sender, DragEventArgs e)
         {
+            //يعني عم قول للويندوز الملف اللي ماسكتو الفارة مسموح ينحط بمكان البكتشر بوكس
             e.Effect = DragDropEffects.Copy;
         }
 
@@ -53,16 +49,112 @@ namespace MultiMediaProject
         {
             //عم ناخد بيانات الصورة اللي جبناها من اللابتوب وزتيناها بالمكان الصح
             var data = e.Data.GetData(DataFormats.FileDrop);
-            if (data !=null) {
+            if (data != null)
+            {
+                //عم نخزن معلومات الصورة ب مصفوفة سترنغات ضمن الفايل نيم
                 var filename = data as string[];
-                if (filename.Length>0) {
+                if (filename.Length > 0)
+                {
                     pictureBox1.Image = Image.FromFile(filename[0]);
                 }
+
+
+
             }
 
         }
+        //convert to HSV
+        private void button2_Click(object sender, EventArgs e)
+        {
 
-      
-      
+
+            Bitmap image = new Bitmap(originalImage);
+
+            Mat rgbImage = image.ToMat();
+
+            Mat HsvImage = new Mat();
+
+            CvInvoke.CvtColor(rgbImage, HsvImage, ColorConversion.Bgr2Hsv);
+
+            pictureBox1.Image = HsvImage.ToBitmap();
+
+        }
+        //convert to YCrCb
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Bitmap image = new Bitmap(originalImage);
+
+            Mat rgbImage = image.ToMat();
+
+            Mat HsvImage = new Mat();
+
+            CvInvoke.CvtColor(rgbImage, HsvImage, ColorConversion.Bgr2YCrCb);
+
+            pictureBox1.Image = HsvImage.ToBitmap();
+        }
+        //convert to YUV
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Bitmap image = new Bitmap(originalImage);
+
+            Mat rgbImage = image.ToMat();
+
+            Mat HsvImage = new Mat();
+
+            CvInvoke.CvtColor(rgbImage, HsvImage, ColorConversion.Bgr2Yuv);
+
+            pictureBox1.Image = HsvImage.ToBitmap();
+
+        }
+        //convert to LAB
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Bitmap image = new Bitmap(originalImage);
+
+            Mat rgbImage = image.ToMat();
+
+            Mat HsvImage = new Mat();
+
+            CvInvoke.CvtColor(rgbImage, HsvImage, ColorConversion.Bgr2Lab);
+
+            pictureBox1.Image = HsvImage.ToBitmap();
+
+        }
+
+        // التحويل إلى نظام CMY (المستخدم للوصول لـ CMYK)
+        private void button6_Click(object sender, EventArgs e)
+        {
+          
+
+            // 1. إنشاء صورة فارغة بنفس أبعاد الصورة الأصلية لتخزين النتيجة
+            Bitmap cmyImage = new Bitmap(originalImage);
+
+            // 2. استخدام حلقات For للمرور على كل بكسل (طولاً وعرضاً)
+            for (int i = 0; i < originalImage.Height; i++)
+            {
+                for (int j = 0; j < originalImage.Width; j++)
+                {
+                    // الحصول على لون البكسل الحالي من الصورة الأصلية
+                    Color pixelColor = originalImage.GetPixel(j, i);
+
+                    // حساب قيم CMY (طرح قيم RGB من 255)
+                    // Cyan = 255 - Red
+                    // Magenta = 255 - Green
+                    // Yellow = 255 - Blue
+                    int c = 255 - pixelColor.R;
+                    int m = 255 - pixelColor.G;
+                    int y = 255 - pixelColor.B;
+
+                    // إنشاء اللون الجديد باستخدام القيم المحسوبة
+                    Color cmyColor = Color.FromArgb(c, m, y);
+
+                    // وضع اللون الجديد في الصورة الناتجة
+                    cmyImage.SetPixel(j, i, cmyColor);
+                }
+            }
+
+            // 3. عرض النتيجة في الـ PictureBox
+            pictureBox1.Image = cmyImage;
+        }
     }
 }
